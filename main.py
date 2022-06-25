53868591cca8ccadbd89452b2924fb158a6763a1
Niklas Sombert <niklas@ytvwld.de>
2022-06-25 12:00:53 +0200

    Add huepy

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+import huepy
+from sys import argv
+
+if len(argv) == 1:
+    print(huepy.bad("Please specify a history file to load"))
+    exit(1)
+
+history_file = Path(argv[1])
+if not history_file.exists() or not history_file.is_file():
+    print(huepy.bad("The specified path does not exist (or it's not regular file)"))
+    exit(1)
